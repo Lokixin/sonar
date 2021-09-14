69a82816dac9db9fe043a2625efb10df8f854bb3
Dimas Ávila <dimasavilamartinez@gmail.com>
2021-09-14 21:04:27 +0200

    main.py updated, ready to be executed

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from Controller import Controller
+
 from constants import BASE_DIR
 
 
 if __name__ == "__main__":
 
     controller = Controller(BASE_DIR)
-    controller.getNextSongTitle("sample.txt")
+    #   Posar aquí el nom del txt amb el títol de la cançó
+    songName = controller.getNextSongTitle("sample.txt")
+    controller.abbleton.playSong(songName)
